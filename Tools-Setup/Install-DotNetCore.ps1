cec5c200ff3c5e42a3c041c0e0b77deb50e6cd7e
HamzaBoukraa <15948623+HamzaBoukraa@users.noreply.github.com>
2020-06-26 14:40:29 +0200

    Fixing SecurityProtocole Issue

--- Tools-Setup/Install-DotNetCore.ps1
+++ Tools-Setup/Install-DotNetCore.ps1
@@ -1,2 +1,3 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';
 ./dotnet-install.ps1;
