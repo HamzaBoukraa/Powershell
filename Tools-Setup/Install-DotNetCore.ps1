8e814daf9cdf15fe9c44efd6e995f10e64ee0cc1
HamzaBoukraa <15948623+HamzaBoukraa@users.noreply.github.com>
2022-10-21 14:24:04 +0200

    Update Install-DotNetCore.ps1

--- Tools-Setup/Install-DotNetCore.ps1
+++ Tools-Setup/Install-DotNetCore.ps1
@@ -1,3 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';
+$WebClient = New-Object Net.WebClient;
+$WebClient.DownloadFile("https://dot.net/v1/dotnet-install.ps1", ".\dotnet-install.ps1");
+# Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile ".\dotnet-install.ps1";
 ./dotnet-install.ps1;
