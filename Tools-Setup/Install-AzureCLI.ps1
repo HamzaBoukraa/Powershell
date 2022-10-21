7a9e0d9ea71ed659b2562695f9bf401e60e6835d
HamzaBoukraa <15948623+HamzaBoukraa@users.noreply.github.com>
2022-10-21 14:22:47 +0200

    Update Install-AzureCLI.ps1

--- Tools-Setup/Install-AzureCLI.ps1
+++ Tools-Setup/Install-AzureCLI.ps1
@@ -1,3 +1,5 @@
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi;
+$WebClient = New-Object Net.WebClient;
+$WebClient.DownloadFile("https://aka.ms/installazurecliwindows", ".\AzureCLI.msi");
+# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi;
 Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet';
 rm .\AzureCLI.msi;
